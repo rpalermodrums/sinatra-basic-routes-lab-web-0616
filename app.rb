01d4e27cfa32d4f9e1560257e2aaea423104a91c
Ryan Palermo <stix250@gmail.com>
2016-06-24 22:59:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Ryan"
+  end
+
+  get '/hometown' do
+    'My hometown is Randolph'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is Milestones'
+  end
 end
